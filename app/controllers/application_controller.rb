83610b0c77e5fb1b43daa19e6a7b0556a1c4b809
Kristen Hazard <kristen@hazardbio.com>
2009-08-04 10:22:06 -0700

    [deploy production] show page carousel fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   helper_method :admin?
   
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   
