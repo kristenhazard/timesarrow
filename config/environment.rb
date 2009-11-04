9d69c692aa2622cf7ae517e515d9d94a7f4a5394
Kristen Hazard <kristen@hazardbio.com>
2009-11-04 11:48:26 -0800

    fix timeline description:

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,7 @@ Rails::Initializer.run do |config|
   config.gem "amazon-ecs", :lib => "amazon/ecs"
   config.gem 'bluecloth', :lib => 'bluecloth'
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
-  # config.gem 'RedCloth'
+  config.gem 'RedCloth'
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
