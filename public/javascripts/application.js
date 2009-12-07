87e35410f8fdbeee24d0b3b02a3879f72a9a2e44
Kristen Hazard <kristen@hazardbio.com>
2009-12-06 16:52:07 -0800

    refactor finalists javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,16 +3,16 @@ function toggle_finalists() {
 	Element.toggle('finalists');
   if (Element.visible('finalists')) {
 		Element.update('toggle_finalists','hide');
-		Element.writeAttribute('toggle_finalist_flag', 'value', 'hide')
+		Element.writeAttribute('toggle_finalists', 'flag', 'hide')
 	}
 	else {
 		Element.update('toggle_finalists','show');
-		Element.writeAttribute('toggle_finalist_flag', 'value', 'show')
+		Element.writeAttribute('toggle_finalists', 'flag', 'show')
 	}
 }
 
 function set_finalists() {
-	var toggle_text = Element.readAttribute('toggle_finalist_flag', 'value')
+	var toggle_text = Element.readAttribute('toggle_finalists', 'flag')
 	if (toggle_text == "hide") {
 		Element.toggle('finalists')
 	}
