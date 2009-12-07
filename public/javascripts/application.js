5c8597faddcb303697a3e85bc3073d58543d7e54
Kristen Hazard <kristen@hazardbio.com>
2009-12-06 16:11:09 -0800

    make finalists functionality more intuitive

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,19 @@
 
 function toggle_finalists() {
 	Element.toggle('finalists');
-  if (Element.visible('finalists')) Element.update('toggle_finalists','hide');
-	else Element.update('toggle_finalists','show');
+  if (Element.visible('finalists')) {
+		Element.update('toggle_finalists','hide');
+		Element.writeAttribute('toggle_finalist_flag', 'value', 'hide')
+	}
+	else {
+		Element.update('toggle_finalists','show');
+		Element.writeAttribute('toggle_finalist_flag', 'value', 'show')
+	}
 }
 
 function set_finalists() {
-	Element.update('toggle_finalists','show')
+	var toggle_text = Element.readAttribute('toggle_finalist_flag', 'value')
+	if (toggle_text == "hide") {
+		Element.toggle('finalists')
+	}
 }
