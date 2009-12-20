60fb8e517afafc92a30007dbe63d4221a0e2ef36
Kristen Hazard <kristen@hazardbio.com>
2009-12-20 09:35:13 -0800

    order of timelines in filter pages

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -91,7 +91,7 @@ class TimelinesController < ApplicationController
     category = params[:category]
     subcategory = params[:subcategory]
     genre = params[:genre]
-    @timelines = Timeline.filtered_cat(category).filtered_subcat(subcategory).filtered_genre(genre).all(:order => 'genre')
+    @timelines = Timeline.filtered_cat(category).filtered_subcat(subcategory).filtered_genre(genre).all(:order => 'genre, name')
     @headertitle = "TIME'S ARROW - Book " + subcategory
     @contenttitle = 'Book ' + subcategory
     self.title = @headertitle
