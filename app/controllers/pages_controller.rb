72ae8d421c1527d927359998d3b7faa77a54950c
Kristen Hazard <kristen@hazardbio.com>
2010-01-25 16:38:43 -0800

    adding pages controller to set title via title helpers

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def privacy
+    self.title = "TIME'S ARROW - Privacy Policy"
+  end
+  def about
+    self.title = "TIME'S ARROW - About"
+  end
+end
